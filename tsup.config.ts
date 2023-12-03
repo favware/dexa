8bc9c52af21b877e2fc7c3cb89d25645bff7cac1
Jeroen Claassens <support@favware.tech>
2023-12-03 12:12:08 +0100

    fix(tsup): better loading of package.json, specify file extension to cjs, bump to es2022

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,21 +1,24 @@
-import { readFile } from 'node:fs/promises';
 import { defineConfig } from 'tsup';
 
-const packageJson = await readFile(new URL('./package.json', import.meta.url), 'utf-8');
-const { dependencies } = JSON.parse(packageJson);
+const {
+  default: { dependencies }
+} = await import('./package.json', {
+  with: { type: 'json' }
+});
 
 export default defineConfig({
   clean: true,
   dts: false,
   entry: ['src/index.ts'],
-  format: ['cjs'],
+  format: 'cjs',
   outDir: 'bundle',
-  target: 'es2021',
+  target: 'es2022',
   tsconfig: 'src/tsconfig.json',
   noExternal: [...Object.keys(dependencies)],
   shims: false,
   sourcemap: false,
   bundle: true,
   minify: true,
-  keepNames: false
+  keepNames: false,
+  outExtension: () => ({ js: '.cjs' })
 });
